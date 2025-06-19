5de6dcd32a1b5b003bfcf13a5bfab69a11b69dfe
yss1235-why <cofclanone1@gmail.com>
2025-06-19 07:33:41 +0530

    Update index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,4 @@
-// src/hooks/index.ts - Updated exports for new architecture
+// src/hooks/index.ts - Updated exports for new architecture with lazy auth
 export { 
   useFirebaseSubscription,
   useGameSubscription,
@@ -7,5 +7,6 @@ export {
   cleanupAllSubscriptions
 } from './useFirebaseSubscription';
 
+export { useLazyAuth } from './useLazyAuth';
 export { useToast } from './use-toast';
 export { useIsMobile } from './use-mobile';
